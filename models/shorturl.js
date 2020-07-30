7097095626e651e25670fd426c720e0cfe7b528a
sanithps98 <pssanith@karunya.edu.in>
2020-07-30 23:46:29 +0530

    Eslint-Prettier Configured

--- models/shorturl.js
+++ models/shorturl.js
@@ -1,21 +1,24 @@
+// This model is going to store all our shorturl infromation
+
 const mongoose = require('mongoose');
 const shortid = require('shortid');
 
 const shorturlSchema = new mongoose.Schema({
-    full: {
-        type: String,
-        required: true
-    },
-    short: {
-        type: String,
-        required: true,
-        default: shortid.generate
-    },
-    clicks: {
-        type: Number,
-        required: true,
-        default: 0
-    }
-})
+  full: {
+    type: String,
+    required: true,
+  },
+  short: {
+    type: String,
+    required: true,
+    default: shortid.generate,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
 
-module.exports = mongoose.model('shorturl',shorturlSchema)
+// eslint-disable-next-line prettier/prettier
+module.exports = mongoose.model('shorturl', shorturlSchema);
--- server.js
+++ server.js
@@ -1,34 +1,51 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const shorturl = require('./models/shorturl');
+
 const app = express();
 
-mongoose.connect('mongodb://admin:admin@cluster0-shard-00-00-ppij2.mongodb.net:27017,cluster0-shard-00-01-ppij2.mongodb.net:27017,cluster0-shard-00-02-ppij2.mongodb.net:27017/URLShortener?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',{
-    useNewUrlParser: true, useUnifiedTopology: true
-});
+mongoose.connect(
+  // eslint-disable-next-line prettier/prettier
+  'mongodb://admin:admin@cluster0-shard-00-00-ppij2.mongodb.net:27017,cluster0-shard-00-01-ppij2.mongodb.net:27017,cluster0-shard-00-02-ppij2.mongodb.net:27017/URLShortener?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 // Express uses jade as its default template engine ,so we would have to tell it to use ejs instead
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false}))
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
-app.get('/', async (req,res) => {
-    const shorturls = await shorturl.find()
-    res.render('index', { shorturls: shorturls });
+app.get('/', async (req, res) => {
+  const shorturls = await shorturl.find();
+  res.render('index', {
+    shorturls: shorturls,
+  });
 });
 
-app.post('/shorturls', async (req,res) => {
-    await shorturl.create({ full: req.body.fullUrl})
-    res.redirect('/')
+app.post('/shorturls', async (req, res) => {
+  await shorturl.create({
+    full: req.body.fullUrl,
+  });
+  res.redirect('/');
 });
 
-app.get('/:shorturl', async (req, res) =>{
-    const shorturl_value = await shorturl.findOne({ short : req.params.shorturl })
-    if(shorturl_value == null) return res.sendStatus(404)
+app.get('/:shorturl', async (req, res) => {
+  const shorturlValue = await shorturl.findOne({
+    short: req.params.shorturl,
+  });
+  if (shorturlValue == null) return res.sendStatus(404);
 
-    shorturl_value.clicks++
-    shorturl_value.save()
+  shorturlValue.clicks += 1;
+  shorturlValue.save();
 
-    res.redirect(shorturl_value.full)
-})
+  res.redirect(shorturlValue.full);
+});
 
-app.listen(process.env.PORT || 4000); 
+// eslint-disable-next-line prettier/prettier
+app.listen(process.env.PORT || 4000);
